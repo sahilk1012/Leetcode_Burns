e1deec951a8a9be301ce11b0d7ba108487c8f3e8
Sahil kumar <148351667+sahilk1012@users.noreply.github.com>
2026-08-01 19:18:36 +0530

    Time: 3 ms (44.33%), Space: 8.5 MB (92.81%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        long rev = 0;
+        int original = x;
+        while (x > 0) {
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+        return rev == original;
+    }
+};
